6eef5977a8d247049a16962c027b060bdf6f932f
topalek <topalek@mail.ru>
2021-04-15 10:54:00 +0300

    cache flush

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -141,6 +141,7 @@ class SiteController extends BaseAdminController
     public function actionClearCache()
     {
         Yii::$app->cache->flush();
-        return $this->goBack();
+        Yii::$app->session->setFlash('success', 'Виконано!');
+        return $this->redirect(Yii::$app->request->referrer);
     }
 }
